55ae6d841a2f8626eb5cb071cee0af1a3f737fe2
Sam Chillcott <sam@samchillcott.com>
2020-10-07 12:19:49 +0100

    updated to htmlFor

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import ReactDOM from "react-dom";
 // create react componenet
 
 const App = () => {
-	return <div>Hiya!</div>;
+    const buttonText = "Click Me"
+	return (
+		<div>
+			<label className="label" htmlFor="name">
+				Enter name:
+			</label>
+			<input id="name" type="text" />
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+		</div>
+	);
 };
 
 // take react componeont and show on screen
+
+ReactDOM.render(<App />, document.querySelector("#root"));
