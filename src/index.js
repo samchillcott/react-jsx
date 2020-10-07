deaaab5af4dd193e05df7a4b30acc666d37e4867
Sam Chillcott <sam@samchillcott.com>
2020-10-07 10:42:36 +0100

    CRA strip

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// impor react and react dom libs
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// create react componenet
+
+const App = () => {
+	return <div>Hiya!</div>;
+};
+
+// take react componeont and show on screen
